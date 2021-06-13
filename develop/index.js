8b5f44dc37fbf867663d5f195f91ef16dc765642
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-06-12 19:59:32 -0700

    table of contents

--- develop/index.js
+++ develop/index.js
@@ -74,8 +74,6 @@ inquirer
     
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 function createREADMEFile(input) {
     let readmeTitle;
     let readmeDescription;
@@ -84,23 +82,49 @@ function createREADMEFile(input) {
     const tocHead = "## Table of Contents";
     let installArr;
     const installHead = "## Installation";
-    let readmeUsage;
-    const usageHead = "## Usage";
-    let readmeContribution;
-    const contributionHead = "## Contribution";
-    let readmeTest;
-    const testingHead = "## Tests";
-    let readmeLicence = input.license;
-    const licenseHead = "## License";
-    let readmeQuestions;
-    const questionsHead = "## Questions";
-    let completeREADME = [];
-
-
-
-
-
-
+    
+    
+    // Adding Title
+    if (input.title == '') {
+        readmeTitle = '# TITLE';
+    } else {
+        readmeTitle = `# ${input.title}`;
+    }
+    completeREADME.push(readmeTitle);
+    
+    
+    //Add in license badge here!!
+    let badge = `![](https://img.shields.io/badge/license-${readmeLicence.replace(/ /g, "%20")}-blue?style=flat-square)`;
+    completeREADME.push(badge);
+    
+    
+    // Adding description
+    if (input.description == '') {
+        readmeDescription = `${descriptionHead}\n Enter project description here.`;
+    } else {
+        readmeDescription = `${descriptionHead}\n${input.description}`;
+    }
+    completeREADME.push(readmeDescription);
+    
+    
+    //Adding Table of Contents
+    tableOfContents = `${tocHead}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
+    completeREADME.push(tableOfContents);
+    
+    
+    //Adding installation instructions
+    completeREADME.push(`${installHead}`);
+    
+    installArr = input.install.split(',').map(item => {
+        return `${item.trim()}`;
+    });
+    
+    for (var i = 0; i < installArr.length; i++) {
+        completeREADME.push(`${i + 1}. ${installArr[i]}`);
+    }
+    
+    
+    
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -108,100 +132,4 @@ function init() {}
 // Function call to initialize app
 init();
 
-
-
-
-
-
-
-// Adding Title
-if (input.title == '') {
-    readmeTitle = '# TITLE';
-} else {
-    readmeTitle = `# ${input.title}`;
-}
-completeREADME.push(readmeTitle);
-
-
-//Add in license badge here!!
-let badge = `![](https://img.shields.io/badge/license-${readmeLicence.replace(/ /g, "%20")}-blue?style=flat-square)`;
-completeREADME.push(badge);
-
-
-// Adding description
-if (input.description == '') {
-    readmeDescription = `${descriptionHead}\n Enter project description here.`;
-} else {
-    readmeDescription = `${descriptionHead}\n${input.description}`;
-}
-completeREADME.push(readmeDescription);
-
-
-//Adding Table of Contents
-tableOfContents = `${tocHead}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
-completeREADME.push(tableOfContents);
-
-
-//Adding installation instructions
-completeREADME.push(`${installHead}`);
-
-installArr = input.install.split(',').map(item => {
-    return `${item.trim()}`;
-});
-
-for (var i = 0; i < installArr.length; i++) {
-    completeREADME.push(`${i + 1}. ${installArr[i]}`);
-}
-
-
-//Adding Usage
-if (input.usage == '') {
-    readmeUsage = `\n${usageHead}\n Enter project usage here.`;
-} else {
-    readmeUsage = `\n${usageHead}\n${input.usage}`;
-}
-completeREADME.push(readmeUsage);
-
-
-//Adding Contributing
-if (input.contribution == '') {
-    readmeContribution = `\n${contributionHead}\n Enter project contriburtion information here.`;
-} else {
-    readmeContribution = `\n${contributionHead}\n${input.contribution}`;
-}
-completeREADME.push(readmeContribution);
-
-
-//Adding Tests
-if (input.testing == '') {
-    readmeTest = `\n${testingHead}\n Enter project testing information here.`;
-} else {
-    readmeTest = `\n${testingHead}\n${input.testing}`;
-}
-completeREADME.push(readmeTest);
-
-
-//License info
-readmeLicence = `\n${licenseHead}\nThis project is convered under the ${input.license}.`;
-completeREADME.push(readmeLicence);
-
-
-//Questions section with github link
-readmeQuestions = `\n${questionsHead}\nFor questions about this project, please see my GitHub at [${input.github}](https://github.com/${input.github}), or reach out by email at ${input.email}.`;
-completeREADME.push(readmeQuestions);
-
-
-//Joining the created README Array with a newline separator
-const README = completeREADME.join('\n');
-    
-
-//Creating the README
-fs.writeFile("./example/README-example.md", README, (err) => {
-    if (err) {
-        throw err;
-    } else {
-        console.log("README file successfully created!");
-    }
-});
-}
-
+// TODO: Create a function to generate markdown for README
